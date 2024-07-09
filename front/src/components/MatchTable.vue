440d1be2797799c15be23d8b91e5d9f1a94f08ef
Simon Mayer <simon@simonmayer.co.uk>
2024-07-09 23:35:35 +0100

    Reroute match action endpoints
    Remove obsolete turnId property
    
    Improve card backing, providing clarity about edge of card, and using a much nicer tartan pattern

--- front/src/components/MatchTable.vue
+++ front/src/components/MatchTable.vue
@@ -90,7 +90,6 @@ export default {
       if (this.match.current_round_id) {
         const data = await roundsService.getCurrentTurn(this.match.current_round_id);
         this.currentTurnUserId = data.user_id;
-        this.turnId = data.turn_id;
         this.currentTurnActions = data.actions || [];
       }
     },
@@ -118,7 +117,7 @@ export default {
       if (this.isCurrentUserTurn && !this.loading && !this.hasDrawAction) {
         this.$emit('loading', true);
         try {
-          const card = await turnsService.drawFromStockPile(this.turnId);
+          const card = await turnsService.drawFromStockPile(this.matchId);
           this.myHand.push(card)
           this.match.stock_pile_size = this.match.stock_pile_size - 1;
           this.currentTurnActions.push({action_type: 'draw'});
