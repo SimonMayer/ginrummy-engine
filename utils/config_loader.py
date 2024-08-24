dac7e368e63d4b10b18a1938fc40e9be1e47c834
Simon Mayer <simon@simonmayer.co.uk>
2024-08-24 05:18:40 +0100

    Use cache for configs loaded from files

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -2,6 +2,10 @@ import json
 import os
 import sys
 
+_cached_database_config = None
+_cached_game_config = None
+_cached_settings_config = None
+
 def load_config(filename):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(script_dir, '../config/', filename)
@@ -10,15 +14,24 @@ def load_config(filename):
         with open(config_path, 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        sys.exit('Database configuration file not found.')
+        sys.exit(f'Configuration file {filename} not found.')
     except json.JSONDecodeError:
-        sys.exit('Error decoding the configuration file.')
+        sys.exit(f'Error decoding the configuration file {filename}.')
 
 def load_database_config():
-    return load_config('database.json')
+    global _cached_database_config
+    if _cached_database_config is None:
+        _cached_database_config = load_config('database.json')
+    return _cached_database_config
 
 def load_game_config():
-    return load_config('game.json')
+    global _cached_game_config
+    if _cached_game_config is None:
+        _cached_game_config = load_config('game.json')
+    return _cached_game_config
 
 def load_settings_config():
-    return load_config('settings.json')
+    global _cached_settings_config
+    if _cached_settings_config is None:
+        _cached_settings_config = load_config('settings.json')
+    return _cached_settings_config
